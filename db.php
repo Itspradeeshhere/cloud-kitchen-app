4b35f40cb53ac44c0680370fda94af2379ff7063
Itspradeeshhere <pradeesh.raj03@gmail.com>
2025-04-08 19:13:22 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
-$host = "sql12.freesqldatabase.com";
-$dbname = "sql12771975";
-$username = "sql12771975";
-$password = "DYPk6Z3QGV";
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$database = getenv('DB_NAME');
 
-$conn = new mysqli($host, $username, $password, $dbname, 3306);
+$conn = new mysqli($host, $user, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
